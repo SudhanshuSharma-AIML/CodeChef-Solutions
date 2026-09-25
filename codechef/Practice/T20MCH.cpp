04c646c13c98cc701adb8c8299dd3ce2c5b37e22
SudhanshuSharma-AIML <sudhanshusharmastu@gmail.com>
2026-09-25 16:11:07 +0530

    Solved: T20MCH (codechef)

---
+++ codechef/Practice/T20MCH.cpp
@@ -0,0 +1,23 @@
+// Problem: T20MCH
+// Platform: codechef
+// Language: C++​
+// Verdict: Accepted
+// URL: https://www.codechef.com/practice/course/logical-problems/DIFF800/problems/T20MCH
+// Solved on: 2026-09-25T10:41:07.488Z
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+     int r,o,c;
+     cin>>r>>o>>c;
+     int rover=20-o;
+     int run=(rover*6*6)+c;
+     if(run>r){
+         cout<<"YES";
+     }
+     else{
+         cout<<"NO";
+     }
+
+}
